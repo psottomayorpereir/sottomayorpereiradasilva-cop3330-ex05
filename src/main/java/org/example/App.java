5bd5a6c2202bbcef42c8af75db0a171162d3b8af
Pedro Henrique Sotto-Mayor Pereira da Silva <itapeu16@hotmail.com>
2021-09-13 03:50:50 -0700

    final

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,3 +1,4 @@
+
 package org.example;
 
 import java.util.Scanner;
@@ -8,21 +9,41 @@ public class App
     {
         Scanner sc= new Scanner(System.in);
         String num1, num2;
-        int n1, n2;
-        int result;
-        System.out.print("What is the first number? ");
-        num1=sc.nextLine();
-        System.out.print("What is the second number? ");
-        num2=sc.nextLine();
-        n1=Integer.parseInt(num1);
-        n2=Integer.parseInt(num2);
-        result=n1+n2;
-        System.out.println(n1 + " + " + n2 + " = " + result);
-        result=n1-n2;
-        System.out.println(n1 + " - " + n2 + " = " + result);
-        result=n1*n2;
-        System.out.println(n1 + " * " + n2 + " = " + result);
-        result=n1/n2;
-        System.out.println(n1 + " / " + n2 + " = " + result);
+        double n1=0.0, n2=0.0;
+        boolean check=true;
+        double add, sub, mul, div;
+        do{
+            System.out.print("What is the first number? ");
+            num1=sc.nextLine();
+            try{
+                n1 = Double.parseDouble(num1);
+                check=true;
+            }catch (NumberFormatException ex) {
+                System.out.println("You have to enter a numeric value!");
+                check=false;
+            }
+            if(n1<0){
+                System.out.println("Do not enter a negative number!");
+            }
+        }while(n1<0 || check==false);
+        do{
+            System.out.print("What is the second number? ");
+            num2=sc.nextLine();
+            try{
+                n2 = Double.parseDouble(num2);
+                check=true;
+            }catch (NumberFormatException ex) {
+                System.out.println("You have to enter a numeric value!");
+                check=false;
+            }
+            if(n2<=0){
+                System.out.println("Do not enter a negative number nor zero for the second number!");
+            }
+        }while(n2<=0 || check==false);
+        add=n1+n2;
+        sub=n1-n2;
+        mul=n1*n2;
+        div=n1/n2;
+        System.out.printf("%.2f + %.2f = %.2f\n%.2f - %.2f = %.2f\n%.2f * %.2f = %.2f\n%.2f / %.2f = %.2f\n", n1,n2,add,n1,n2,sub,n1,n2,mul,n1,n2,div);
     }
 }
